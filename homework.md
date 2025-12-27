4ce3cebabe47a1280d546b561a11a9c579b73bdb
tarunsaha2424 <tarunsaha2424@gmail.com>
2025-12-27 12:13:37 +0530

    writing-auth-middleware

--- homework.md
+++ homework.md
@@ -21,3 +21,15 @@
  - Use of regex in routes /a/, /.*fly$/
  - Reading the query params in the routes 
  - Reading the dynamic routes
+
+
+ - Multiple ROute Handlers - play with the code
+ - next()
+ - next function and errors along with res.send()
+ - app.use("/route", rh,[rh2, rh3], rh4, rh5);
+ - What is a Middleware? Why do we need it? 
+ - How express JS basically handles requests behind the scenes
+ - Difference between app.use and app.all
+ - Write a dummy auth middleware for admin
+ - Write a dummy auth middleware for all user routes, except /user /login
+ 
--- src/app.js
+++ src/app.js
@@ -2,11 +2,58 @@ const express = require("express");
 const app = express();
 
 
+const {adminAuth, userAuth} = require("./middlewares/auth");
+
+
+app.use("/admin", adminAuth);
+
+
+app.post("user/login", ()=> {
+  res.send("User logged in successfully!");
+});
+
+// app.use("/admin", (req,res,next)=>{
+//   const token = "xyzalksdkd";
+//   const isAdminAuthorized =token === "xyz";
+//   if (!isAdminAuthorized){
+//     res.status(401).send("Unauthorized request");
+//   }
+//   else{
+//     next();
+//   }
+// })
+
+
+app.get("/user",userAuth,(req,res)=>{
+  res.send("User Data Sent");
+})
+
 // app.use("/user",(req,res)=>{
 //   res.send("Hahaha");
 
 // });
+app.get("/admin/getAllData",(req, res)=>{
 
+  res.send("All Data Sent");
+  // Check if the request is authorized
+  // const token = "xyzasf";
+  // const isAdminAuthorized = token === "xyz";
+  // if(isAdminAuthorized){
+  //   res.send("All Data Sent");
+
+  // }else{
+  //   res.status(401).send("Unauthorized request");
+  // }
+
+  // res.send("All Data Sent");  
+});
+
+
+app.get("/admin/deleteUser",(req, res)=>{
+  // Logic of fetching all data
+
+  res.send("Deleted a User");  
+})
 
 // This will only handle GET call to /user
 // app.get("/ab*cd", (req,res)=>{
@@ -23,34 +70,52 @@ const app = express();
 // app.use("/route", rh,[rh2, rh3], rh4,rh5 );
 
 
-app.use("/user",[(req,res, next)=>{
-  //Route Handler
-  //  res.send("Route Handler 1");
-  console.log("Handling the route user!!" );
-  next();
-  // res.send("Route Handler 1");  
-},
-(req, res, next)=>{
-  console.log("Handling the route user 2!! ");
-  // res.send("2nd Response!!");
-  next();
-}],
-(req, res,next)=>{
-  console.log("Handling the route user 3!! ");
-  // res.send("3rd Response!!");
-next();
-},
-(req, res, next )=>{
-  console.log("Handling the route user 4!! ");
-  // res.send("4th Response!!");
-  next();
-},
-(req, res, next )=>{
-  console.log("Handling the route user 5!! ");
-  res.send("5th Response!!");
-  // next();
-}
-);
+// app.use("/user",(req,res, next)=>{
+//   //Route Handler
+//   //  res.send("Route Handler 1");
+//   console.log("Handling the route user!!" );
+//   next();
+//   // res.send("Route Handler 1");  
+// }
+// // (req, res, next)=>{
+// //   console.log("Handling the route user 2!! ");
+// //   // res.send("2nd Response!!");
+// //   next();
+// // }],
+// // (req, res,next)=>{
+// //   console.log("Handling the route user 3!! ");
+// //   // res.send("3rd Response!!");
+// // next();
+// // },
+// // (req, res, next )=>{
+// //   console.log("Handling the route user 4!! ");
+// //   // res.send("4th Response!!");
+// //   next();
+// // },
+// // (req, res, next )=>{
+// //   console.log("Handling the route user 5!! ");
+// //   res.send("5th Response!!");
+// //   // next();
+// // }
+// );
+
+// app.get("/user",(req,res, next)=>{
+//   //Route Handler
+//   //  res.send("Route Handler 1");
+//   // console.log("Handling the route user!!" );
+//   next();
+//   // res.send("Route Handler 1");  
+// },
+// (req,res,next)=>{
+//   next();
+// },
+// (req,res,next)=>{
+//   next();
+// },(req,res, next)=>{
+//   res.send("2nd Route Handler");
+// }); 
+
+
 // app.post("/user", (req,res)=>{
 //   // console.log("Save Data to the database");
 //   // Save data
